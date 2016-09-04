af960501dd011e81bc058aa3ee54629395cfc556
myron <qf846140903@gmail.com>
2016-09-04 15:05:47 +0800

    test .gitignore

---
+++ src/min_max_loss.py
@@ -0,0 +1,7 @@
+import caffe
+import numpy as np
+from numpy import *
+import yaml
+from multiprocessing import Process, Queue
+from caffe._caffe import RawBlobVec
+from sklearn import preprocessing
